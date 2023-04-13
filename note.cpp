5a894e38c09594d2a5dbda090c9b38f78cad6964
poteitou <apoteitou@gmail.com>
2023-04-13 10:56:41 +0700

    add:   note about color in SFML

---
+++ note.cpp
@@ -0,0 +1,12 @@
+sf::Color
+{
+    sf::Color black       = sf::Color::Black;
+    sf::Color white       = sf::Color::White;
+    sf::Color red         = sf::Color::Red;
+    sf::Color green       = sf::Color::Green;
+    sf::Color blue        = sf::Color::Blue;
+    sf::Color yellow      = sf::Color::Yellow;
+    sf::Color magenta     = sf::Color::Magenta;
+    sf::Color cyan        = sf::Color::Cyan;
+    sf::Color transparent = sf::Color::Transparent;
+}
