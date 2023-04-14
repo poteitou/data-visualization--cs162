712ba67cfd5d1ac76846f89579b03dc72cc3492b
poteitou <apoteitou@gmail.com>
2023-04-14 08:15:33 +0700

    modified:   change size of buttons

--- screen.cpp
+++ screen.cpp
@@ -3,13 +3,13 @@
 Screen::Screen(sf::RenderWindow& window, sf::Font& font)
 : mWindow(window)
 {
-    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(100, 300), sf::Color::Cyan, sf::Color::Blue, "Static Array", font, 25));
-    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(100, 500), sf::Color::Cyan, sf::Color::Blue, "Dynamic Array", font, 25));
-    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(600, 300), sf::Color::Cyan, sf::Color::Blue, "Singly Linked List", font, 25));
-    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(600, 500), sf::Color::Cyan, sf::Color::Blue, "Doubly Linked List", font, 25));
-    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(600, 700), sf::Color::Cyan, sf::Color::Blue, "Circular Linked List", font, 25));
-    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(1200, 300), sf::Color::Cyan, sf::Color::Blue, "Stack", font, 25));
-    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(1200, 500), sf::Color::Cyan, sf::Color::Blue, "Queue", font, 25));
+    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(200, 300), sf::Color::Cyan, sf::Color::Blue, "Static Array", font, 30));
+    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(200, 500), sf::Color::Cyan, sf::Color::Blue, "Dynamic Array", font, 30));
+    mButton.push_back(Button(mWindow, sf::Vector2f(400, 100), sf::Vector2f(600, 300), sf::Color::Cyan, sf::Color::Blue, "Singly Linked List", font, 30));
+    mButton.push_back(Button(mWindow, sf::Vector2f(400, 100), sf::Vector2f(600, 500), sf::Color::Cyan, sf::Color::Blue, "Doubly Linked List", font, 30));
+    mButton.push_back(Button(mWindow, sf::Vector2f(400, 100), sf::Vector2f(600, 700), sf::Color::Cyan, sf::Color::Blue, "Circular Linked List", font, 30));
+    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(1100, 300), sf::Color::Cyan, sf::Color::Blue, "Stack", font, 30));
+    mButton.push_back(Button(mWindow, sf::Vector2f(300, 100), sf::Vector2f(1100, 500), sf::Color::Cyan, sf::Color::Blue, "Queue", font, 30));
 }
 
 void Screen::draw()
