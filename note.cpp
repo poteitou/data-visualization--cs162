16c985310b398e399de695afbacef547375ecf91
poteitou <apoteitou@gmail.com>
2023-05-02 09:06:50 +0700

    modified:   more colors

--- note.cpp
+++ note.cpp
@@ -10,11 +10,11 @@ sf::Color
     sf::Color cyan        = sf::Color::Cyan;
     sf::Color transparent = sf::Color::Transparent;
 
-    light orange pastel:    255, 200, 200
+    light orange pastel:    255, 200, 200 - 255, 105, 105
     purple pastel:  200, 200, 255
     grey:           200, 200, 200
 
-    light blue pastel:          180, 225, 255
+    light blue pastel:          180, 225, 255 - 115, 200, 255
     little dark blue pastel:    120, 190, 235
     orange pastel:   255, 130, 130
 }
